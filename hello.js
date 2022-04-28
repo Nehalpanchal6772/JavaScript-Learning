910a828ce6e9f05af6ece5139866c4b8157f47cb
Nehalpanchal6772 <16bce166@nirmauni.ac.in>
2022-04-28 15:42:21 +0530

    This is first file of my JavaScript

---
+++ hello.js
@@ -0,0 +1,10 @@
+// console.log("Hello World");
+
+console.log ("Hello JS Learner"); // USING DOUBLE DOUTE
+
+console.log ('single quote'); // USING SINGLE QUOTE
+
+console.log (`using this sign`); // USING BACK TICKS
+
+// ALL ABOVE DECLARED FORMATES ARE VALID WITHOUT SEMICOLON AT THE END
+// SPACE AFTER CONSOLE.LOG IS ALSO VALID
